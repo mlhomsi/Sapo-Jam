eb19b53554f0034acf691c7d119f4d4a448596cb
mlhomsi <mlhomsi@gmail.com>
2020-09-25 16:48:53 -0300

    morte funcionando

---
+++ Sapo Jam/Assets/Kill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(2);
+        //Debug.Log("morte");
+    }
+}
