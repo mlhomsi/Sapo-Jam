e1cef8bdaf4b630aa7ef1d0b57f126d62288722f
Carol <carol.brandao01@hotmail.com>
2020-09-25 21:21:22 -0300

    adicionando a morte por queda e p inimigos, sistema de vidas esta quase implementado mas comentado no kill.cs

---
+++ Sapo Jam/Assets/Scripts/Kill.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //int life;
+    //private Rigidbody2D rb;
+    void Start()
+    {
+        //rb = this.GetComponent<Rigidbody2D>();
+        //life = 50;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Fall")
+            SceneManager.LoadScene(2);
+        if(col.gameObject.tag == "Enemy")
+        {
+
+            //life -= 1;
+            //UnityEngine.Debug.Log(life);
+            //if (life >= 0)
+            //{
+            //  rb.AddForce(transform.up * thrust);
+            //}
+            // else
+            //{
+            SceneManager.LoadScene(2);
+            //}
+
+        }
+    }
+}
