2aef333f1cfd2f4d19e6eed725a3687f88dee9be
mlhomsi <mlhomsi@gmail.com>
2020-09-25 22:04:00 -0300

    Endgame

---
+++ Sapo Jam/Assets/Scripts/PostCutscene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PostCutscene : MonoBehaviour
+{
+    private void Start()
+    {
+        {
+            Application.Quit();
+        }
+    }
+
+}
